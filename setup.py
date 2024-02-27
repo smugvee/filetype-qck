57bc7a6a65382a7d0727890787bb17057f437d9d
smugvee <85663797+smugvee@users.noreply.github.com>
2024-02-26 21:59:15 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='qcktypes',
+    author='smugvee',
+    description='Python implementation of dyne's file extension list',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/smugvee/qcktypes',
+    version="1.0",
+    packages=find_packages(),
+    install_requires=requirements,
+classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.3'
+)
