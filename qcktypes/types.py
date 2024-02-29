b153a35bb3ea64eff86edc24b5172fe6520987e0
smugvee <3@3.3>
2024-02-28 19:44:41 -0500

    Add json to code category

--- qcktypes/types.py
+++ qcktypes/types.py
@@ -5,7 +5,7 @@ def handle_type(file_extension):
     "archive": "7z,a,apk,ar,bz2,cab,cpio,deb,dmg,egg,gz,iso,jar,lha,mar,pea,rar,rpm,s7z,shar,tar,tbz2,tgz,tlz,war,whl,xpi,zip,zipx,xz,pak",
     "audio": "aac,aiff,ape,au,flac,gsm,it,m3u,m4a,mid,mod,mp3,mpa,pls,ra,s3m,sid,wav,wma,xm",
     "book": "mobi,epub,azw1,azw3,azw4,azw6,azw,cbr,cbz",
-    "code": "1.ada,2.ada,ada,adb,ads,asm,bas,bash,bat,c,c++,cbl,cc,class,clj,cob,cpp,cs,csh,cxx,d,diff,e,el,f,f77,f90,fish,for,fth,ftn,go,groovy,h,hh,hpp,hs,html,htm,hxx,java,js,jsx,jsp,ksh,kt,lhs,lisp,lua,m,m4,nim,patch,php,pl,po,pp,py,r,rb,rs,s,scala,sh,swg,swift,v,vb,vcxproj,xcodeproj,xml,zsh",
+    "code": "1.ada,2.ada,ada,adb,ads,asm,bas,bash,bat,c,c++,cbl,cc,class,clj,cob,cpp,cs,csh,cxx,d,diff,e,el,f,f77,f90,fish,for,fth,ftn,go,groovy,h,hh,hpp,hs,html,htm,hxx,java,js,json,jsx,jsp,ksh,kt,lhs,lisp,lua,m,m4,nim,patch,php,pl,po,pp,py,r,rb,rs,s,scala,sh,swg,swift,v,vb,vcxproj,xcodeproj,xml,zsh",
     "exec": "exe,msi,bin,command,sh,bat,crx,bash,csh,fish,ksh,zsh",
     "font": "eot,otf,ttf,woff,woff2",
     "image": "3dm,3ds,max,bmp,dds,gif,jpg,jpeg,png,psd,xcf,tga,thm,tif,tiff,yuv,ai,eps,ps,svg,dwg,dxf,gpx,kml,kmz,webp",
